0183eb2ced937e7123a04780b79f929c58e21026
Kyle <rrblack701@gmail.com>
2025-10-02 19:57:52 +0900

    Fixed header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,9 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
+import {ArchiveNavigation} from "@/app/_components/archive-navigation";
 
-export function ArchiveNavigation() {
-  return (
-    <div className="mb-16 -mt-8"> 
-    <h1 className ="text-center mb-8 text-white font-bold text-4xl"> Looking for past blogs? 
-      </h1>
-    <nav className="flex justify-center space-x-4 mb-8 text-xl">
-      <a
-        href="http://myjapantrip2019.wordpress.com/#"
-        className="text-red-600 hover:text-red-700 font-semibold"
-      >
-        2019 Blog (First time in Japan)
-      </a>
-      <a
-        href="https://kylesjapanlife.wordpress.com/"
-        className="text-red-600 hover:text-red-700 font-semibold"
-      >
-        2022 Blog (Covers my life in Japan from July 2022- January 2024)
-      </a>
-        </nav>
-    </div>
-  );
-}
+
 export default function Index() {
   const allPosts = getAllPosts();
 
