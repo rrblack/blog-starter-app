2088f4e8b2400e4f8def71be21e99a0182ffe79f
Kyle <rrblack701@gmail.com>
2025-10-02 20:01:05 +0900

    Tracking

---
+++ src/app/_components/archive-navigation.tsx
@@ -0,0 +1,22 @@
+export function ArchiveNavigation() {
+  return (
+    <div className="mb-16 -mt-8"> 
+    <h1 className ="text-center mb-8 text-white font-bold text-4xl"> Looking for past blogs? 
+      </h1>
+    <nav className="flex justify-center space-x-4 mb-8 text-xl">
+      <a
+        href="http://myjapantrip2019.wordpress.com/#"
+        className="text-red-600 hover:text-red-700 font-semibold"
+      >
+        2019 Blog (First time in Japan)
+      </a>
+      <a
+        href="https://kylesjapanlife.wordpress.com/"
+        className="text-red-600 hover:text-red-700 font-semibold"
+      >
+        2022 Blog (Covers my life in Japan from July 2022- January 2024)
+      </a>
+        </nav>
+    </div>
+  );
+}
