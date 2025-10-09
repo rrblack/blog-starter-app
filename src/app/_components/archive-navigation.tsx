3d0a5bd6f9a352e16e3e895e08daaa5651ac13c4
rrblack <okaneookii@gmail.com>
2025-10-10 02:33:47 +0900

    Favicon and UI changes

--- src/app/_components/archive-navigation.tsx
+++ src/app/_components/archive-navigation.tsx
@@ -3,22 +3,23 @@ export function ArchiveNavigation() {
     <div className="md:mb-16 -mt-24 md:-mt-8"> 
     <h1 className ="text-center mb-8 text-white font-bold text-4xl"> Looking for past blogs? 
       </h1>
-    <nav className="flex justify-center space-x-4 mb-8 text-wrap md:text-2xl border-x-15 rounded-full shadow-md md:py-5 shadow-red-700">
+    <nav className="flex justify-center flex-wrap space-x-11 md:space-x-8 mb-8 text-wrap md:text-2xl rounded-full shadow-md py-5 md:py-5 border border-red-400 shadow-red-700/50 hover:shadow-red-400/90 transition-shadow-900 ">
       <a
         href="http://myjapantrip2019.wordpress.com/#"
         className="text-red-600 hover:text-red-700 font-semibold"
       >
         2019 Blog (First time in Japan)
       </a>
-      <div>
-        |
+      <div className="hidden md:block">
       </div>
-      <a
+      <div className="mt-5 md:mt-0">
+      <a 
         href="https://kylesjapanlife.wordpress.com/"
         className="text-red-600 hover:text-red-700 font-semibold"
       >
-        2022 Blog (Covers my life in Japan from July 2022- January 2024)
+        2022 Blog (Covers my life in Japan from July 2022 - January 2024)
       </a>
+      </div>
         </nav>
     </div>
   );
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ export default function AboutPage() {
         <Header />
         <div className="md:mt-36 mt-2"> 
           <h1 className="md:-mt-20 text-center text-4xl md:text-8xl mb-12"> Hi I'm Kyle </h1>
+        <div className="md:mt-20"> 
         <Image
         src="/assets/blog/authors/Img_2025_02_16_00_19_00.jpeg"
         alt="Picture of the author"
@@ -16,12 +17,13 @@ export default function AboutPage() {
         height={200}
         className="rounded-l-full mx-auto mt-10">
         </Image>
+        </div>
       <p className="flex items-center md:text-center md:text-xl md:text-wrap mt-10 md:mt-10 mx-5 md:mx-60 mb-10 md:mb-15"> 
         Thanks for stopping in on my blog! Originally from Chattanooga Tennessee, I first came to Japan in July 2022 through the JET Programme, spending an unforgettable year in the beautiful town of Muroto, Kōchi Prefecture. That time deepened my love for Japan’s culture, language, and people. <br/>
 
         <br/> After a year in the country side of Kōchi, I moved to the hectic and busy city of Tokyo to pursue a career in IT. I'm currently working in the tech industry, where I apply skills I taught myself through self-study. The same goes for Japanese—through consistent study and immersion, I earned the JLPT N1, the highest level of Japanese proficiency. <br/> 
 
-        <br/> This blog is where I share my journey, experiences, and thoughts on life in Japan, language learning, and working in tech. Thanks for stopping by!<br/> </p>
+        <br/> This blog is where I share my journey, experiences, and thoughts on life in Japan, language learning, and working in tech. Stay tuned for more interesting stories!<br/> </p>
         </div> 
         </Container>
     </main>
