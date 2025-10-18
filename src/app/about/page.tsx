6af88e6c684baa9c4c5d8b8e4682f86fb4e24246
rrblack <okaneookii@gmail.com>
2025-10-18 10:11:06 +0900

    About page fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
       <Container >
         <Header />
         <div className="md:mt-36 mt-2"> 
-          <h1 className="md:-mt-20 text-center text-4xl md:text-8xl mb-12"> Hi, I'm Kyle </h1>
+          <h1 className="md:-mt-20 text-center text-7xl md:text-8xl mb-12"> Hi, I'm Kyle </h1>
         <div className="md:mt-20"> 
         <Image
         src="/assets/blog/authors/Img_2025_02_16_00_19_00.jpeg"
@@ -18,7 +18,7 @@ export default function AboutPage() {
         className="rounded-l-full mx-auto mt-10">
         </Image>
         </div>
-      <p className="flex items-center md:text-center md:text-xl md:text-wrap mt-10 md:mt-10 mx-5 md:mx-60 mb-10 md:mb-15"> 
+      <p className="flex items-center md:text-center text-lg md:text-xl md:text-wrap mt-10 md:mt-10 mx-5 md:mx-60 mb-10 md:mb-15"> 
         Thanks for stopping in on my blog! Originally from Chattanooga Tennessee, I first came to Japan in July 2022 through the JET Programme, spending an unforgettable year in the beautiful town of Muroto, Kōchi Prefecture. That time deepened my love for Japan’s culture, language, and people. <br/>
 
         <br/> After a year in the country side of Kōchi, I moved to the hectic and busy city of Tokyo to pursue a career in IT. I'm currently working in the tech industry, where I apply skills I taught myself through self-study. The same goes for Japanese—through consistent study and immersion, I earned the JLPT N1, the highest level of Japanese proficiency. <br/> 
