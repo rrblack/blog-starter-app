bfb109f5694f7c684c6614e0a97750f39915ca91
rrblack <okaneookii@gmail.com>
2025-11-02 02:16:30 +0900

    Shimmering loading for posts

--- src/app/_components/mdximage.tsx
+++ src/app/_components/mdximage.tsx
@@ -1,35 +1,34 @@
 "use client";
 
-import Image from "next/image";
+import Image, { ImageProps } from "next/image";
+import { useState } from "react";
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+type MDXImageProps = ImageProps & {
+  className?: string;
+  width?: number;
+  height?: number;
+};
 
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+export function MDXImage({
+  width = 700,
+  height = 475,
+  className,
+  ...props
+}: MDXImageProps) {
+  const [isLoading, setLoading] = useState(true);
 
-export function MDXImage({ width = 700, height = 475, ...props }: any) {
   return (
-    <Image 
-      width={width}
-      height={height}
-      {...props} 
-      unoptimized
-      placeholder="blur"
-      blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(width, height))}`}
-    />
+    <div className={`sm:mx-0 ${className ?? ""}`}>
+      <div className="relative aspect-video w-full overflow-hidden rounded-lg shadow-sm">
+        {isLoading && <div className="absolute inset-0 skeleton" />}
+        <Image
+          width={width}
+          height={height}
+          {...props}
+          unoptimized
+          onLoad={() => setLoading(false)}
+        />
+      </div>
+    </div>
   );
 }
